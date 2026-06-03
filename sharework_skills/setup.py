d769280d3e9172c3eed64aa0135f005a8d64b744
Manuel Beschi <ManuelBeschi@users.noreply.github.com>
2026-06-03 22:54:49 +0200

    feat(bt): register bt_node entry point in setup.py

--- sharework_skills/setup.py
+++ sharework_skills/setup.py
@@ -7,18 +7,12 @@ package_name = 'sharework_skills'
 setup(
     name=package_name,
     version='0.0.0',
-    # Trova automaticamente la cartella sharework_skills e la installa come pacchetto Python
     packages=find_packages(exclude=['test']),
     data_files=[
-        # Ament index + package.xml
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-
-        # Installazione dei file di launch
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-
-        # Installazione dei file di configurazione YAML
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
@@ -33,12 +27,12 @@ setup(
         ],
     },
     entry_points={
-        # Unificato in un unico dizionario per non sovrascrivere le chiavi
         'console_scripts': [
             'test_skills = sharework_skills.test_skills:main',
             'test_app = sharework_skills.test_app:main',
             'test_app_loop = sharework_skills.test_app_loop:main',
-            'node = sharework_skills.main:main'  # <-- Corretto: punta a main.py
+            'node = sharework_skills.main:main',
+            'bt_node = sharework_skills.bt_pipeline:main',
         ],
     },
 )
